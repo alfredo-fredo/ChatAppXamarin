265b01871eb221a861737c9c53e880dc05a834b7
alfredo-fredo <alfred98.sav@gmail.com>
2020-11-09 13:06:16 +0100

    began main layout

---
+++ XamarinChatApp/ListViewTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+namespace XamarinChatApp
+{
+    public class ListViewTemplate
+    {
+        public Image ProfilePicture { get; set; }
+        public String Name { get; set; }
+        public String MessageHint { get; set; }
+        public String MessageTime { get; set; }
+
+        public ListViewTemplate()
+        {
+        }
+    }
+}
---
+++ XamarinChatApp/MyPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinChatApp
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
