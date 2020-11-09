a4ee40327d269d4854d2a2fe43a858efe5a10aab
alfredo-fredo <alfred98.sav@gmail.com>
2020-11-09 13:13:40 +0100

    began layout for messages + search bar

--- XamarinChatApp/MainPage.xaml.cs
+++ XamarinChatApp/MainPage.xaml.cs
@@ -13,6 +13,53 @@ namespace XamarinChatApp
         public MainPage()
         {
             InitializeComponent();
+
+            MainListView.ItemsSource = new List<ListViewTemplate>
+            {
+            new ListViewTemplate
+                {
+                ProfilePicture = null,
+                Name = "Farah",
+                    MessageHint = "I'm doing well! How about you?",
+                    MessageTime = "8:28 pm"               
+               },
+               new ListViewTemplate
+                {
+                ProfilePicture = null,
+                Name = "William",
+                    MessageHint = "I'm doing well! How about you?",
+                    MessageTime = "9:32 am"
+               },
+               new ListViewTemplate
+                {
+                ProfilePicture = null,
+                Name = "Alfred",
+                    MessageHint = "I'm doing well! How about you?",
+                    MessageTime = "1:02 am"
+               },
+           };
         }
+
+        /*async private void MainListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var Selected = e.Item as ListViewTemplate;
+
+            switch (Selected.OrderNumber)
+            {
+                case 1:
+                    await Navigation.PushAsync(new MyPage());
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+
+         ((ListView)sender).SelectedItem = null;
+
+
+        }*/
     }
 }
